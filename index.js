6858e04c14508afb4af42bf4299ee6e2f8ac9323
Srijal225 <106398268+Srijal225@users.noreply.github.com>
2024-04-10 22:42:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var mongoose = require("mongoose")
+const express = require("express")
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
-const app= express()
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+const app = express ();
+dotenv.config();
 
-mongoose.connect(mongodb+srv://sankhladheeraj575:bwkv6Y9AKyLv7Xrz@sankhla1.ghbtj5b.mongodb.net/MoneyTracker)
-var db = mongoose.connection
-db.on('error', ()=> console.log("Error in connecting to the Database"))
-db.once('open', () => console.log("Connected to Database"))
+const port = process.env.PORT || 3000;
 
-app.post("/add", (req,res) =>{
-    var category_select = req.body.category_select
-    var amount_input= req.body.amount_input
-    var info = req.body.info
-    var date_input = req.body.date_input
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
 
-    var data={
-        "Category": category_select,
-        "Amount" : amount_input,
-        "Info": info,
-        "Date": date_input
-    }
-    db.collection('users').insertOne(data, (err,collection) => {
-        if(err){
-            throw err;
-        }
-        console.log("Record Inserted Successfully")
+mongoose.connect(mongodb+srv://${username}:${password}@cluster0.wxmyirm.mongodb.net/registrationFormDB, {
+    serverSelectionTimeoutMS: 5000, 
+});
 
-    })
+const registrationSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String
+});
+
+const registration = mongoose.model("Registration", registrationSchema);
+
+app.use(bodyParser.urlencoded ({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html");
 })
-app.get("/",(req,res) =>{
-    res.set({
-        "Allow-access-Allow-Origin":'*'
-    })
-    return res.redirect('index.html')
-}).listen(3000)
 
-console.log("Listening on port 3000")
+app.post("/register", async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+
+const existingUser = await registration.findOne({email : email });
+if(!existingUser) {
+    const registrationData = new registration({
+            name, 
+            email,
+            password,
+        });
+        await registrationData.save(); 
+        res.redirect("/success");
+    }
+    else{
+        console.log("User alreadyexist");
+        res.redirect("/error");
+    }
+
+    }    
+    catch (error) {
+    console.log(error);
+    res.redirect("error");
+
+     }    
+});
+
+app.get("/success", (req, res)=>{
+    res.sendFile (__dirname+"/pages/success.html");
+})
+app.get("/error", (req, res)=>{
+    res.sendFile (__dirname+"/pages/error.html");
+})
+app.listen(port, ()=>{
+    console.log(server is running on port ${port});
+})
+    
